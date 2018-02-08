19ec1d39800ce917d8eda9fb7d81a3dcf48c36de
NkhumeleniTendani <36258963+NkhumeleniTendani@users.noreply.github.com>
2018-02-08 13:56:26 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,30 @@
-// var myImage = document.querySelector('img');
-//
-// myImage.onclick = function() {
-//     var mySrc = myImage.getAttribute('src');
-//     if(mySrc === 'images/meag2.jpg') {
-//       myImage.setAttribute ('src','images/meag2.jpg');
-//     } else {
-//       myImage.setAttribute ('src','images/meag2.jpg');
-//     }
-// }
-//
-// //User Name Code
-// var myButton = document.querySelector('button');
-// var myHeading = document.querySelector('h1');
+var myImage = document.querySelector('img');
 
-// function setUserName() {
-//   var myName = prompt('Please enter your name.');
-//   localStorage.setItem('name', myName);
-//   myHeading.textContent = 'Hello World!, ' + myName;
-// }
-//
-// if(!localStorage.getItem('name')) {
-//   setUserName();
-// } else {
-//   var storedName = localStorage.getItem('name');
-//   myHeading.textContent = 'Welcome Back, ' + storedName;
-// }
-//
-// myButton.onclick = function() {
-//   setUserName();
-// }
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/meag2.jpg') {
+      myImage.setAttribute ('src','images/meag2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/meag2.jpg');
+    }
+}
 
- var iceCream ='Banana';
- if(iceCream ==='Chocolate'){
-   alert('Yeepy i like it');
- } else{
-   alert('Aww nah no thanks');
- }
+//User Name Code
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello World!, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome Back, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
