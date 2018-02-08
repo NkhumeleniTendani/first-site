50103440b5cedf18862ff8d94477a38a2f38a5dd
NkhumeleniTendani <36258963+NkhumeleniTendani@users.noreply.github.com>
2018-02-08 10:34:35 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+// var myImage = document.querySelector('img');
+//
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/meag2.jpg') {
+//       myImage.setAttribute ('src','images/meag2.jpg');
+//     } else {
+//       myImage.setAttribute ('src','images/meag2.jpg');
+//     }
+// }
+//
+// //User Name Code
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
+
+// function setUserName() {
+//   var myName = prompt('Please enter your name.');
+//   localStorage.setItem('name', myName);
+//   myHeading.textContent = 'Hello World!, ' + myName;
+// }
+//
+// if(!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   var storedName = localStorage.getItem('name');
+//   myHeading.textContent = 'Welcome Back, ' + storedName;
+// }
+//
+// myButton.onclick = function() {
+//   setUserName();
+// }
+
+ var iceCream ='Banana';
+ if(iceCream ==='Chocolate'){
+   alert('Yeepy i like it');
+ } else{
+   alert('Aww nah no thanks');
+ }
